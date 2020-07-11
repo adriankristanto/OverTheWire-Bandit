3539beee466a1d687f1476f9ac4d949b1c34dcce
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-11 22:56:26 +1000

    the password will now be printed by a different function instead of the print_stdout function

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import time
+import string
 
 
 ADDRESS = 'bandit.labs.overthewire.org'
@@ -6,13 +7,10 @@ PORT = 2220
 SLEEP = 1
 
 
-def print_stdout(stdout, passwd=0):
+def print_stdout(stdout):
     """
     stdout: obtained from stdout output of paramiko's client.exec_command()\n
-    passwd: determine whether a password needs to be returned
     """
-    password = None
-
     # time.sleep() added as a workaround for known paramiko bug
     # https://github.com/paramiko/paramiko/issues/1617
     time.sleep(SLEEP)
@@ -21,9 +19,19 @@ def print_stdout(stdout, passwd=0):
 
     for line in stdout:
         line = line.strip('\n')
-        password = line if passwd else None
         print(line)
 
     print("#" * 50 + '\n')
 
-    return password
+def print_password(stdout):
+    """
+    stoud: password returned by the remote server
+    """
+    time.sleep(SLEEP)
+
+    print("#" * 50)
+    
+    # remove any whitespaces from the password
+    print(f"{stdout.translate('', '', string.whitespace)}")
+
+    print("#" * 50)
