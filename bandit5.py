d1e11c734bcdbc732244d50751ab8cd27c1caa22
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-11 23:19:16 +1000

    added the guide for bandit5 -> bandit6

--- bandit5.py
+++ bandit5.py
@@ -4,3 +4,38 @@ import utils
 USERNAME = 'bandit5'
 PASSWORD = 'koReBOKuIDDepwhWk7jZC0RTdopnAYKh'
 
+# initialisation
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# connecting to the remote server
+print('connecting...\n')
+client.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
+
+print('ls -la')
+stdin, stdout, stderr = client.exec_command('ls -la')
+utils.print_stdout(stdout)
+
+print('cd inhere && ls -la')
+stdin, stdout, stderr = client.exec_command('cd inhere && ls -la')
+utils.print_stdout(stdout)
+
+# we need a file of size 1033 bytes, which is represented with c in find
+# it must not be executable, so, we need ! -executable, where ! means not
+# finally, it must be human readable, therefore, we need to use grep ASCII after executing file command
+print('find ./inhere -type f -size 1033c ! -executable -exec file {} + | grep ASCII')
+stdin, stdout, stderr = client.exec_command('find ./inhere -type f -size 1033c ! -executable -exec file {} + | grep ASCII')
+utils.print_stdout(stdout)
+
+# read the file
+print('cat ./inhere/maybehere07/.file2')
+stdin, stdout, stderr = client.exec_command('cat ./inhere/maybehere07/.file2')
+
+stdout = stdout.readlines()
+password = utils.get_password(stdout)
+utils.print_stdout(stdout)
+
+# bandit6 password: DXjZPULLxYr17uwoI01bNLQbtFemEgo7
+print(f"bandit6 password: {password}")
+
+client.close()
