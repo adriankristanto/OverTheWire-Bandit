721d36bdb8592750e407cbfa17f68f0e0feb7c5e
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-14 22:12:45 +1000

    only connect to the remote server if data.txt has not been downloaded

--- bandit12/bandit12.py
+++ bandit12/bandit12.py
@@ -10,22 +10,23 @@ import scp
 USERNAME = 'bandit12'
 PASSWORD = '5Te8Y4drgCRfCx8ugdwuEX8KFC6k2EUu'
 
-client = paramiko.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-print('connectin...')
-client.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
-
 if 'data.txt' not in os.listdir(os.path.dirname(os.path.realpath(__file__))):
+    # only connect to the remote server if data.txt has not been downloaded
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    print('connecting...')
+    client.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
+
     print('downloading data.txt...')
     # create an SCP client to download data.txt to work on it on the local machine
     # as this challenge is more complex
     scp_client = scp.SCPClient(client.get_transport())
     scp_client.get('data.txt')
+
     scp_client.close()
+    client.close()
 else:
     print("data.txt has been downloaded")
-
-client.close()
 
 
