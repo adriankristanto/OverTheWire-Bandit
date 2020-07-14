a540e9c1dd0b6f20631d1b5819de8bc5666959f8
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-14 22:06:10 +1000

    using scp to download the data from the remote server

--- bandit12/bandit12.py
+++ bandit12/bandit12.py
@@ -7,3 +7,25 @@ import utils
 # https://pypi.org/project/scp/
 import scp
 
+USERNAME = 'bandit12'
+PASSWORD = '5Te8Y4drgCRfCx8ugdwuEX8KFC6k2EUu'
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+print('connectin...')
+client.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
+
+if 'data.txt' not in os.listdir(os.path.dirname(os.path.realpath(__file__))):
+    print('downloading data.txt...')
+    # create an SCP client to download data.txt to work on it on the local machine
+    # as this challenge is more complex
+    scp_client = scp.SCPClient(client.get_transport())
+    scp_client.get('data.txt')
+    scp_client.close()
+else:
+    print("data.txt has been downloaded")
+
+client.close()
+
+
