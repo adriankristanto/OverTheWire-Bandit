c2a01adbd085764fbfcb76a713cd7546aafa7e59
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-17 23:40:25 +1000

    added the code to download bandit14 privatekey file and use it for authentication

--- bandit13/bandit13.py
+++ bandit13/bandit13.py
@@ -14,6 +14,7 @@ client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 client.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
 
+# download the private key of bandit14
 scp_client = scp.SCPClient(client.get_transport(), progress=utils.progress)
 scp_client.get('sshkey.private')
 
@@ -22,4 +23,18 @@ print('\n')
 scp_client.close()
 client.close()
 
+# connecting as bandit14 using the downloaded ssh private key
+bandit14_client = paramiko.SSHClient()
+bandit14_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# reference: https://stackoverflow.com/questions/54612609/paramiko-not-a-valid-rsa-private-key-file
+privatekey = paramiko.RSAKey.from_private_key_file('sshkey.private')
+bandit14_client.connect(hostname=utils.ADDRESS, port=utils.PORT, username='bandit14', pkey=privatekey)
+
+print('cat /etc/bandit_pass/bandit14')
+# read bandit14 password stored in /etc/bandit_pass/bandit14
+_, stdout, _ = bandit14_client.exec_command('cat /etc/bandit_pass/bandit14')
+utils.print_stdout(stdout)
+
+bandit14_client.close()
 os.remove('sshkey.private')
