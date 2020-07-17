89c62f3331de553d033d1de6a9e2ded6ccc919e4
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-17 23:29:20 +1000

    added the code to download the ssh private key of bandit14

--- bandit13/bandit13.py
+++ bandit13/bandit13.py
@@ -8,3 +8,18 @@ import utils
 
 USERNAME = 'bandit13'
 PASSWORD = '8ZjyCRiBWFYkneahHwxCv3wb2a1ORpYL'
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+client.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
+
+scp_client = scp.SCPClient(client.get_transport(), progress=utils.progress)
+scp_client.get('sshkey.private')
+
+print('\n')
+
+scp_client.close()
+client.close()
+
+os.remove('sshkey.private')
