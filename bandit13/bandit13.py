3eda0b054db50eb012ff4e3a8aae91e65e862080
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-17 23:44:04 +1000

    added the guide for bandit13 -> bandit14

--- bandit13/bandit13.py
+++ bandit13/bandit13.py
@@ -11,9 +11,10 @@ PASSWORD = '8ZjyCRiBWFYkneahHwxCv3wb2a1ORpYL'
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
+print('connecting as bandit13...\n')
 client.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
 
+print('downloading bandit14 private key file...\n')
 # download the private key of bandit14
 scp_client = scp.SCPClient(client.get_transport(), progress=utils.progress)
 scp_client.get('sshkey.private')
@@ -27,6 +28,7 @@ client.close()
 bandit14_client = paramiko.SSHClient()
 bandit14_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+print('connecting as bandit14...\n')
 # reference: https://stackoverflow.com/questions/54612609/paramiko-not-a-valid-rsa-private-key-file
 privatekey = paramiko.RSAKey.from_private_key_file('sshkey.private')
 bandit14_client.connect(hostname=utils.ADDRESS, port=utils.PORT, username='bandit14', pkey=privatekey)
@@ -34,7 +36,12 @@ bandit14_client.connect(hostname=utils.ADDRESS, port=utils.PORT, username='bandi
 print('cat /etc/bandit_pass/bandit14')
 # read bandit14 password stored in /etc/bandit_pass/bandit14
 _, stdout, _ = bandit14_client.exec_command('cat /etc/bandit_pass/bandit14')
+stdout = stdout.readlines()
 utils.print_stdout(stdout)
+password = utils.get_password(stdout)
+
+# bandit14 password: 4wcYUJFw0k0XLShlDzztnTBHiqxU3b3e
+print(f'bandit14 password: {password}')
 
 bandit14_client.close()
 os.remove('sshkey.private')
