a2e92c9fe319e74356e93a9257d9e4f3a241befd
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-18 17:40:51 +1000

    start with bandit20 -> bandit21

---
+++ bandit20/bandit20.py
@@ -0,0 +1,17 @@
+import os
+import sys
+sys.path.insert(1, os.path.dirname(os.path.realpath(__file__)) + '/..')
+
+import paramiko
+import utils
+
+USERNAME = 'bandit20'
+PASSWORD = 'GbKksEFF4yrVs6il55v6gwY5aVje5f0j'
+
+client1 = paramiko.SSHClient()
+client1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+print('connecting...\n')
+client1.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
+
+client1.close()
