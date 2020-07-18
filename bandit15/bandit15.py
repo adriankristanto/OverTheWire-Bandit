d3cd8e2a7ae9f871ce175813d4cbeb94296cbbab
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-18 12:22:28 +1000

    start with bandit15 -> bandit16

---
+++ bandit15/bandit15.py
@@ -0,0 +1,9 @@
+import os
+import sys
+sys.insert(1, os.path.dirname(os.path.realpath(__file__)) + '/..')
+
+import paramiko
+import utils
+
+USERNAME = 'bandit15'
+PASSWORD = 'BfMYroe26WYalil77FoDi9qh59eK5xNr'
