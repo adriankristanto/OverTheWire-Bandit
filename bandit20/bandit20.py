c38b29598353e65c06eb8f0fa88f972e22de1d23
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-18 17:41:45 +1000

    creating a second SSH client

--- bandit20/bandit20.py
+++ bandit20/bandit20.py
@@ -11,7 +11,12 @@ PASSWORD = 'GbKksEFF4yrVs6il55v6gwY5aVje5f0j'
 client1 = paramiko.SSHClient()
 client1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+client2 = paramiko.SSHClient()
+client2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
 print('connecting...\n')
 client1.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
+client2.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
 
 client1.close()
+client2.close()
