685a20489f89202d4ee84d09513d2a7bf126f1ce
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-20 20:10:12 +1000

    added the guide for bandit22 -> bandit23

--- bandit22/bandit22.py
+++ bandit22/bandit22.py
@@ -15,12 +15,18 @@ print('connecting...\n')
 client.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
 
 print('ls -l /etc/cron.d')
+_, stdout, _ = client.exec_command('ls -l /etc/cron.d')
+utils.print_stdout(stdout)
 
 # read the cronjob of bandit23
 print('cat /etc/cron.d/cronjob_bandit23')
+_, stdout, _ = client.exec_command('cat /etc/cron.d/cronjob_bandit23')
+utils.print_stdout(stdout)
 
 # read the executed script
 print('cat /usr/bin/cronjob_bandit23.sh')
+_, stdout, _ = client.exec_command('cat /usr/bin/cronjob_bandit23.sh')
+utils.print_stdout(stdout)
 
 # firstly, the script would generate the md5 hash of the 
 # sentence: "I am user $myname"
@@ -31,6 +37,13 @@ print('cat /usr/bin/cronjob_bandit23.sh')
 # and we only want the first column
 # finally, we know that the filename is the md5 hash stored in the tmp directory,
 # we can make it into a variable and read it immediately
-print("cat /tmp/$(echo I am user bandit23 | md5sum | cut  -d ' ' -f 1)")
+print("cat /tmp/$(echo I am user bandit23 | md5sum | cut -d ' ' -f 1)")
+_, stdout, _ = client.exec_command("cat /tmp/$(echo I am user bandit23 | md5sum | cut -d ' ' -f 1)")
+stdout = stdout.readlines()
+utils.print_stdout(stdout)
+password = utils.get_password(stdout)
+
+# bandit23 password: jc1udXuA1tiHqjIsL8yaapX5XIAI6i0n
+print(f'bandit23 password: {password}')
 
 client.close()
