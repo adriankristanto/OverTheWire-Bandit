627783e5a0f8ae6f51d063b04f04b10ac2fad4f5
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-20 20:01:30 +1000

    added the commands that will be executed in the remote server

--- bandit22/bandit22.py
+++ bandit22/bandit22.py
@@ -10,3 +10,27 @@ PASSWORD = 'Yk7owGAcWjwMVRwrTesJEwB7WVOiILLI'
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+print('connecting...\n')
+client.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
+
+print('ls -l /etc/cron.d')
+
+# read the cronjob of bandit23
+print('cat /etc/cron.d/cronjob_bandit23')
+
+# read the executed script
+print('cat /usr/bin/cronjob_bandit23.sh')
+
+# firstly, the script would generate the md5 hash of the 
+# sentence: "I am user $myname"
+# since we are trying to get the password of bandit23, we can replace $myname with bandit23
+# then we get the md5 hash of the sentence "I am user bandit23" with
+# echo I am user bandit23 | md5sum | cut -d ' ' -f 1
+# because md5sum would output: 8ca319486bfbbc3663ea0fbe81326349  -
+# and we only want the first column
+# finally, we know that the filename is the md5 hash stored in the tmp directory,
+# we can make it into a variable and read it immediately
+print("cat /tmp/$(echo I am user bandit23 | md5sum | cut  -d ' ' -f 1)")
+
+client.close()
