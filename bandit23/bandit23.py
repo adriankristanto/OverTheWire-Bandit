cc18690af3983a267d837a5d95180e08b0c5bb74
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-21 21:43:13 +1000

    added detailed explanation of the cronjob configuration and the sh file

--- bandit23/bandit23.py
+++ bandit23/bandit23.py
@@ -13,4 +13,41 @@ client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 print('connecting...\n')
 client.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
+
+print('ls -l /etc/cron.d/')
+
+print('cat /etc/cron.d/cronjob_bandit24')
+# note that according to https://crontab.guru/#*_*_*_*_*
+"""
+* * * * * bandit24 /usr/bin/cronjob_bandit24.sh &> /dev/null
+"""
+# the cronjob is executed every minute
+
+print('cat /usr/bin/cronjob_bandit24.sh')
+
+# firstly, 
+# cd /var/spool/$myname
+# echo "Executing and deleting all scripts in /var/spool/$myname:"
+# we can check in /var/spool that there exists bandit24 directory
+# which allows anyone to write into and execute it
+"""
+bandit23@bandit:~$ ls -l /var/spool/
+total 12
+drwxrwx-wx 33 root bandit24 4096 Jul 21 13:13 bandit24
+"""
+# next, 
+"""
+if [ "$i" != "." -a "$i" != ".." ];
+then
+    echo "Handling $i"
+    owner="$(stat --format "%U" ./$i)"
+    if [ "${owner}" = "bandit23" ]; then
+        timeout -s 9 60 ./$i
+    fi
+    rm -f ./$i
+fi
+"""
+# it will execute each file inside of it, except current directory (.) and parent directory(..)
+# if the owner of the file is bandit23, it will execute it for 60 seconds
+
 client.close()
