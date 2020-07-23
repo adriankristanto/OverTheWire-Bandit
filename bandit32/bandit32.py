9ae3449edb161162b70d7dbeab5e2a10fc300647
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-23 22:02:17 +1000

    execute arbitrary command

--- bandit32/bandit32.py
+++ bandit32/bandit32.py
@@ -4,6 +4,7 @@ sys.path.insert(1, os.path.dirname(os.path.realpath(__file__)) + '/..')
 
 import paramiko
 import utils
+import time
 
 USERNAME = 'bandit32'
 PASSWORD = '56a9bf19c63d650ce78e6ec0354ee45e'
@@ -11,5 +12,17 @@ PASSWORD = '56a9bf19c63d650ce78e6ec0354ee45e'
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+print('connecting...\n')
 client.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
+remote= client.invoke_shell()
+
+output = remote.recv(65535)
+print(output.decode())
+
+remote.send('ls')
+remote.send('\n')
+time.sleep(1)
+output = remote.recv(65535)
+print(output.decode())
+
 client.close()
