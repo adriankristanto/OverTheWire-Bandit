0543d5da4c9c6b0d1e656fb1e3ee699dc4296ccc
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-23 20:05:38 +1000

    establishing connection with the remote server

--- bandit29/bandit29.py
+++ bandit29/bandit29.py
@@ -10,3 +10,9 @@ PASSWORD = 'bbc96594b4e001778eee9975372716b2'
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+client.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
+
+
+
+client.close()
