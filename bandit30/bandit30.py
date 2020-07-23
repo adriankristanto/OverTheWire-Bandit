6240c532aca4b17dfdf7085e33e2f538904e0d8b
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-23 20:44:10 +1000

    cloned the repo

--- bandit30/bandit30.py
+++ bandit30/bandit30.py
@@ -13,4 +13,19 @@ PASSWORD = '5b90576bedb2cc04c86a9e924ce42faf'
 print('connecting...\n')
 client.connect(hostname=utils.ADDRESS, port=utils.PORT, username=USERNAME, password=PASSWORD)
 
+FOLDERNAME = str(uuid.uuid4())
+REPONAME = 'ssh://bandit30-git@localhost/home/bandit30-git/repo'
+print(f"folder name: {FOLDERNAME}\n")
+
+stdin, stdout, _ = client.exec_command(f"mkdir /tmp/{FOLDERNAME} && cd /tmp/{FOLDERNAME} && git clone {REPONAME}")
+stdin.write('yes\n')
+time.sleep(1)
+stdin.write(f'{PASSWORD}\n')
+stdin.flush()
+utils.print_stdout(stdout)
+
+# clean up
+print('deleting temporary directory...')
+client.exec_command(f'rm -rf /tmp/{FOLDERNAME}')
+
 client.close()
