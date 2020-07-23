d60bc703ce19757dd9a98014de54ee5dafd41b53
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-07-23 21:07:40 +1000

    start with bandit31 -> bandit32

---
+++ bandit31/bandit31.py
@@ -0,0 +1,11 @@
+import os
+import sys
+sys.path.insert(1, os.path.dirname(os.path.realpath(__file__)) + '/..')
+
+import paramiko
+import utils
+import time
+import uuid
+
+USERNAME = 'bandit31'
+PASSWORD = '47e603bb428404d265f59c42920d81e5'
